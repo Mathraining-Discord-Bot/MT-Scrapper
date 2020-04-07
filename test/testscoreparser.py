d1cecc923d1561274e2f7a6deb2fc1edf1db51dd
Charlotte THOMAS <contact@nwa2coco.fr>
2020-04-07 15:59:41 +0200

    revert test

--- test/testscoreparser.py
+++ test/testscoreparser.py
@@ -2,47 +2,36 @@ import Mathraining as MT
 import time as t
 
 
-def test_parser(i: int, new=False):
+def test_parser(i: int):
     MTS = MT.User(i)
-    if (new == True):
-        return MTS.score_new()
-    else:
-        return MTS.score()
+    return MTS.score()
 
 
-def compare(i: int):
-    MTS = MT.User(i)
-    time = t.time()
-    print(MTS.score(), "tested old in", (t.time() - time) * 1000, "ms")
-    time = t.time()
-    print(MTS.score_new(), "tested new in", (t.time() - time) * 1000, "ms")
-
-
-def alltest(new=False):
+def alltest():
     time = t.time()
     print("Testing parse score for user 4574")
-    print(test_parser(4574, new))
+    print(test_parser(4574))
     print("Total time", (t.time() - time) * 1000, "ms")
     time = t.time()
     print("Testing parse score for user 4574")
-    print(test_parser(4574, new))
+    print(test_parser(4574))
     print("Total time", (t.time() - time) * 1000, "ms")
     time = t.time()
     print("Testing parse score for Administrator, user 5")
-    print(test_parser(5, new))
+    print(test_parser(5))
     print("Total time", (t.time() - time) * 1000, "ms")
     time = t.time()
     print("Testing parse score for novice, user 4575")
-    print(test_parser(4575, new))
+    print(test_parser(4575))
     print("Total time", (t.time() - time) * 1000, "ms")
     time = t.time()
     print("Testing parse score for unexisting user, user 20000")
-    print(test_parser(20000, new))
+    print(test_parser(20000))
     print("Total time", (t.time() - time) * 1000, "ms")
 
 
 if __name__ == "__main__":
+    time = t.time()
     print("old all test")
     alltest()
-    print("new all test")
-    alltest(True)
+    print("All test executed in ", (t.time()-time)*1000, "ms")
